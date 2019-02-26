cf52cd72e25310f6768159a2110aab65ecda8f69
daniel <39582546+neunzeichen@users.noreply.github.com>
2019-02-26 13:34:43 +0100

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -3,9 +3,10 @@
  * Default settings for DataTables plugin
  *
  * @author Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * 2019-02-26 (neunzeichen): changed default to 1 
  */
 
 
 $conf['excludedPages']      = '(wiki|playground)';
-$conf['enableForAllTables'] = 0;
-$conf['enableLocalization'] = 0;
+$conf['enableForAllTables'] = 1;
+$conf['enableLocalization'] = 1;
